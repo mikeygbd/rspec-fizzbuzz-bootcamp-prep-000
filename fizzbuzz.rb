acd68f30c63503742b28358e791bb4649a9f77c4
Michael Soares <mikey.soares@gmail.com>
2018-10-26 19:42:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(3)
+def fizzbuzz(int)
  if the int % 3 == 3
   puts  "Fizz"
 end
